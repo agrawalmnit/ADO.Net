9a727e3aee7246face3854287e3e07172327d16c
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-19 18:25:24 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,102 @@
+﻿//using System;
+
+//using System.Collections.Generic;
+
+//using System.Linq;
+
+//using System.Text;
+
+//using System.Threading.Tasks;
+
+//using System.Data;
+
+//using System.Data.SqlClient;
+
+//namespace ConsoleApp1
+
+//{
+//    class Class2
+
+//    {
+//        static void Main(string[] args)
+
+//        {
+
+//            Class2.Connection();
+
+//            Console.ReadLine();
+//        }
+//        static void Connection()
+
+//        {
+//            string cs = "Data Source=ABJIMA-ISHANSI; Initial Catalog=ado_db;Integrated Security=true;";
+
+//            SqlConnection con = null;
+
+//            try
+
+//            {
+//                using (con = new SqlConnection(cs))
+
+//                {
+
+
+
+//                    string query = "GetPersons";
+
+
+//                    SqlCommand cmd = new SqlCommand();
+
+//                    cmd.CommandText = query;
+
+//                    cmd.Connection = con;
+
+//                    cmd.CommandType = CommandType.StoredProcedure;
+
+
+//                    con.Open();
+
+//                    SqlDataReader dr = cmd.ExecuteReader();
+
+//                    while (dr.Read())
+
+//                    {
+
+//                        Console.WriteLine("PersonId =" + dr["PersonId"] +
+
+//                            "FirstName = " + dr["FirstName"] +
+
+//                            "LastName = " + dr["LastName"] +
+
+//                            "City =" + dr["City"]);
+
+//                    }
+
+//                }
+
+//            }
+
+
+
+
+//            catch (SqlException ex)
+
+//            {
+
+//                Console.WriteLine(ex.Message);
+
+//            }
+
+//            finally
+
+//            {
+
+//                con.Close();
+
+//            }
+
+//        }
+
+//    }
+
+//}
---
+++ ConsoleApp1/ConsoleApp1/Class4.cs
@@ -0,0 +1,119 @@
+﻿//using System;
+
+//using System.Collections.Generic;
+
+//using System.Linq;
+
+//using System.Text;
+
+//using System.Threading.Tasks;
+
+//using System.Data;
+
+//using System.Data.SqlClient;
+
+//namespace ConsoleApp1
+
+//{
+//    class Class4
+
+//    {
+//        static void Main(string[] args)
+
+//        {
+
+//            Class4.Connection();
+
+//            Console.ReadLine();
+//        }
+//        static void Connection()
+
+//        {
+//            string cs = "Data Source=ABJIMA-ISHANSI; Initial Catalog=ado_db;Integrated Security=true;";
+
+//            SqlConnection con = null;
+
+//            try
+
+//            {
+//                using (con = new SqlConnection(cs))
+
+//                {
+//                    Console.WriteLine("Person ID:");
+
+//                    string PersonId = Console.ReadLine();
+
+
+
+//                    string query = "delete from dbo.Persons where PersonID = @PersonID";
+
+
+
+
+//                    SqlCommand cmd = new SqlCommand(query, con);
+
+
+//                    cmd.Parameters.AddWithValue("@PersonID", PersonId);
+
+
+
+//                    con.Open();
+
+//                    int a = cmd.ExecuteNonQuery();
+
+//                    if (a > 0)
+//                    {
+//                        Console.WriteLine("data has been deleted successfully");
+//                    }
+
+//                    else
+//                    {
+//                        Console.WriteLine("Data deletion failed..");
+//                        }
+
+//                        SqlDataReader dr = cmd.ExecuteReader();
+
+//                    while (dr.Read())
+
+//                    {
+
+//                        Console.WriteLine("PersonId =" + dr["PersonId"] +
+
+//                            "FirstName = " + dr["FirstName"] +
+
+//                            "LastName = " + dr["LastName"] +
+
+//                            "City =" + dr["City"]);
+
+                        
+
+//                    }
+
+//                }
+
+//            }
+
+
+
+
+//            catch (SqlException ex)
+
+//            {
+
+//                Console.WriteLine(ex.Message);
+
+//            }
+
+//            finally
+
+//            {
+
+//                con.Close();
+
+//            }
+
+//        }
+
+//    }
+
+//}
